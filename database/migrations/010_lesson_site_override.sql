f1adf248c44369686aa1ad19d41be3170fbabf84
Tiabaldoo <yakvanval@gmail.com>
2026-09-16 17:56:06 +1100

    Remove incorrectly numbered lesson site migration

--- database/migrations/010_lesson_site_override.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE lessons
-  ADD COLUMN site_override_id BIGINT UNSIGNED NULL AFTER site_id_snapshot,
-  ADD KEY ix_lessons_site_override_id (site_override_id),
-  ADD CONSTRAINT fk_lessons_site_override FOREIGN KEY (site_override_id) REFERENCES sites(id);
